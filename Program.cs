983dcc04b1b960d24865a43dd10769d6dff273bf
Patrick Jørgensen <patrick.jorgensen@kghcustoms.com>
2020-12-01 21:16:32 +0100

    Adding from root folder, not in initail commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Day 1
+            //Day1.Day1.SolvePartOne();
+            Day1.Day1.SolvePartTwo();
+            #endregion Day 1
+
+
+        }
+    }
+}
