63c407564278e15c71048430f819e6dc7b34ce1b
Patrick Jørgensen <patrick.jorgensen@kghcustoms.com>
2020-12-01 21:14:50 +0100

    Initial commit + Day 1

---
+++ Day 1/Day1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AoC2020.Day1
+{
+    static class Day1
+    {
+        public static void SolvePartOne()
+        {
+            List<string> stringValues = File.ReadAllLines(@"C:\Work\AoC\2020\AoC2020\Day 1\Day1Input.txt").ToList<string>();
+            List<int> intValues = stringValues.Select(s => Convert.ToInt32(s)).ToList();
+            foreach (int line in intValues)
+            {
+                if (intValues.Find(z => z == 2020 - line) > 0)
+                {
+                    Console.WriteLine("So {0} and {1} gives 2020", 2020 - line, line);
+                    Console.WriteLine("Multiplied they give: {0}", (2020 - line) * line);
+                    return;
+                }
+            }
+        }
+
+        public static void SolvePartTwo()
+        {
+            List<string> stringValues = File.ReadAllLines(@"C:\Work\AoC\2020\AoC2020\Day 1\Day1Input.txt").ToList<string>();
+            List<int> intValues = stringValues.Select(s => Convert.ToInt32(s)).ToList();
+            foreach (int line in intValues)
+            {
+                foreach (int newLine in intValues)
+                {
+                    if (line + newLine < 2020 && line + newLine > 0)
+                    {
+                        if (intValues.Find(z => z == 2020 - line - newLine) > 0)
+                        {
+                            Console.WriteLine("So {0}, {1} and {2} gives 2020", 2020 - line - newLine, line, newLine);
+                            Console.WriteLine("Multiplied they give: {0}", (2020 - line - newLine) * line * newLine);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
